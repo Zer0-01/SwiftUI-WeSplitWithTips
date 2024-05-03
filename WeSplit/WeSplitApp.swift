7ef87852a1e7b48f0dace4506a33aa0836df4da0
Anas Zulkifli bin Mohd Jeffry <anas.zulkifli2001@gmail.com>
2024-05-03 16:15:04 +0800

    first commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Silentmode Sdn Bhd on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
